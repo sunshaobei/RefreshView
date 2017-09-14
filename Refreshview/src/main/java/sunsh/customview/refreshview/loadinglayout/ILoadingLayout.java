471219c6dfbc620a9224e14aa7c85f0779167617
sunshaobei <sunshaobei@shaozi.com>
2017-09-14 14:57:10 +0800

    Initial commit

---
+++ Refreshview/src/main/java/sunsh/customview/refreshview/loadinglayout/ILoadingLayout.java
@@ -0,0 +1,42 @@
+package sunsh.customview.refreshview.loadinglayout;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sunsh on 2017/4/2.
+ */
+
+public interface ILoadingLayout {
+    /**
+     * make your loading view!
+     * @return
+     */
+    View createLoadingHeader(Context context, ViewGroup parent);
+    View createLoadingFooter(Context context, ViewGroup parent);
+
+    /**
+     * init your views or reset them.
+     */
+    void initAndResetHeader();
+    void initAndResetFooter();
+
+    /**
+     * called when you are pulling the XRefreshLayout, you
+     * can make your pulling animation!
+     */
+    void onPullHeader(float percent);
+    void onPullFooter(float percent);
+
+    /**
+     * called when release a view form pull state!
+     */
+    void onHeaderRefreshing();
+    void onFooterRefreshing();
+    void onHeaderRefreshingComplete();
+    void onFooterRefreshingComplete();
+    void onHeaderRefreshingComplete(String message);
+    void onFooterRefreshingComplete(String message);
+
+}
